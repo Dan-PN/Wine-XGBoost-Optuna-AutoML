defe81b9fbb7ba90dd566f577b57674c711ed306
Dapn <52420127+Dapn@users.noreply.github.com>
2021-05-10 22:21:09 +0100

    added recent files

--- XGBoost_predict.py
+++ XGBoost_predict.py
@@ -1,39 +1,39 @@
 import pandas as pd
 import numpy as np
 import xgboost
+import collections
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import RepeatedKFold
 
-# Prep data
-wines = pd.read_csv("winemag-data-130k-v2.csv")
-wines.columns
-wines.drop(columns='Unnamed: 0', inplace=True)
-wines.dropna(axis='index', subset=['price'], inplace=True)
-wines.drop_duplicates(inplace=True)
-
+# %%
+# ----------------------
 # define objective
 # drop description,taster_twitter_handle also
 # Extract year from title
 # Extract other details
+wines = pd.read_csv("wines_encoded.csv", encoding='utf-8')
+
 data_X = wines.drop(columns='points', inplace=False).values
 data_Y = wines["points"].values
 
 # model
-model = xgboost.XGBRegressor()
+model = xgboost.XGBRegressor(verbosity=2)
 
 # fit
 model.fit(data_X, data_Y)
+print("Model Fitted")
 
 # cross val based on train/test
 cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=42)
 
 # evaluate
 scores = cross_val_score(
-    model, X, y, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1)
+    model, data_X, data_Y, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-2, verbose=2)
 
 print("MAE: {a}".format(a=abs(scores)))
 
 
 # %%
-wines.drop(columns='points', inplace=False)
+#wines.drop(columns='points', inplace=False)
+
 # %%
